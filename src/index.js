be7522ed15dddb62bfd9ca72a4871068ced31278
irinka-irinka <80964268+irinka-irinka@users.noreply>
2022-02-22 13:50:26 +0200

    fix: implement function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function reverse (n) {
-  
+  return n
+      .toString()
+      .replace("-", "")
+      .split("")
+      .slice("")
+      .reverse()
+      .join("")
+      .trim("");
 }
+ 
